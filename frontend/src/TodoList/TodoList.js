c9175eb2dc6ded2f3b1bb4f49f4d6214110522aa
Bùi Đức Thắng <ducthang1341@gmail.com>
2024-11-25 08:55:15 +0700

    fix-pagination

--- frontend/src/TodoList/TodoList.js
+++ frontend/src/TodoList/TodoList.js
@@ -2,37 +2,43 @@ import "./TodoList.css";
 import "../css/icon.css";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
+import Form from "react-bootstrap/Form";
 import Pagination from "react-bootstrap/Pagination";
 import { useDispatch, useSelector } from "react-redux";
 import { showToastMessage } from "../reducer/toastSlice";
-import { toggleComplete } from "../reducer/todoTableSlice";
-import { getTaskList, deleteTask } from "../api/api";
+import { getTaskList, deleteTask, toggleCheckBox } from "../api/api";
 import { useState, useEffect } from "react";
 
 function TodoList({ toggleAddItemForm }) {
   const dispatch = useDispatch();
   // Lấy thông tin người đăng nhập hiện tại
   const user = useSelector((state) => state.handleLogin.loggedUserInfo);
-  // order là số thứ tự hàng hiện trên bảng
-  let order = 0;
-
+  
+  let order = 0; // order là số thứ tự hàng hiện trên bảng
   // taskList là danh sách các hàng lấy từ database
-  const [taskList, setTaskList] = useState([]);
-  /**
-   * Hàm getTodoItem lấy dữ liệu từ db về và lọc ra những kết quả phù hợp với người đăng nhập
-   */
-  const getTodoItem = async () => {
-    try {
-      const response = await getTaskList();
-      response.filter((task) => task.userId === user.id);
-      setTaskList(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [taskList, setTaskList] = useState([]); 
+  const handleTaskList = (tasks) => {
+    setTaskList(tasks);
+  }
+
+  const [currentPage,setCurrentPage] = useState(1); // Trang hiện tại
+  const [numberItemAPage, setNumberItemAPage] = useState(5); // Số item 1 trang
+  const indexOfLastItem = currentPage*numberItemAPage; // index vị trí cuối cùng của trang hiện tại 
+  const indexOfFirstItem = indexOfLastItem - numberItemAPage; // index vị trí đầu tiên của trang hiện tại
+  const numberOfPages = Math.ceil(taskList.length / numberItemAPage); // Tổng số trang
+  const pages = []; // mảng các trang (1, 2, 3,... )
+    for (let i = 1; i <= numberOfPages; i++) {
+      pages.push(i);
+    };
+  
+  const handleCurrentPage = (page) => {
+    setCurrentPage(page);
+  }
+  const handleNumberItemAPage = (num) => {
+    setNumberItemAPage(num);
+  }
   /**
    * Hàm deleteTodoItem gọi api xóa (lưu ở file api.js) nhận param là id hiện tại của task
-   *  id
    */
   const deleteTodoItem = async (id) => {
     await deleteTask(id);
@@ -45,57 +51,57 @@ function TodoList({ toggleAddItemForm }) {
       })
     );
   };
+
+  /**
+   * Hàm getTodoItem lấy dữ liệu từ db về và lọc ra những kết quả phù hợp với người đăng nhập
+   */
+  const getTodoItem = async () => {
+    try {
+      const response = await getTaskList();
+      response.filter((task) => task.userId === user.id);
+      setTaskList(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getTodoItem();
-  }, [taskList]);
+    if (numberOfPages && numberOfPages < currentPage) handleCurrentPage(numberOfPages);
+  },);
 
+  // lấy các task theo trang được phân
+  const paginatedList = taskList.slice(indexOfFirstItem, indexOfLastItem);
   // rowList: chuyển dữ liệu tasklist thành html để hiện ra giao diện
   const rowList =
-    taskList.map((task) => {
+    paginatedList.map((task) => {
       return (
         <tr key={task.id}>
-          <td
-            className={`table-checkbox ${task.completed ? "task-completed" : ""
-              }`}
-          >
+          <td className={`table-checkbox ${task.completed ? "task-completed" : ""}`}>
             <input
+              className="task-checkbox"
               type="checkbox"
               value={task.id}
               defaultChecked={task.completed}
-              onChange={() => handleCompleteCheck(task.id, !task.completed)}
+              onChange={() => toggleCheckBox(task.id, task.completed)}
             ></input>
           </td>
-          <td
-            className={`table-order ${task.completed ? "task-completed" : ""}`}
-          >
+          <td className={`table-order ${task.completed ? "task-completed" : ""}`}>
             {++order}
           </td>
-          <td
-            className={`table-title ${task.completed ? "task-completed" : ""}`}
-          >
+          <td className={`table-title ${task.completed ? "task-completed" : ""}`}>
             {task.title}
           </td>
-          <td
-            className={`table-content ${task.completed ? "task-completed" : ""
-              }`}
-          >
+          <td className={`table-content ${task.completed ? "task-completed" : ""}`}>
             {task.content}
           </td>
-          <td
-            className={`table-deadline ${task.completed ? "task-completed" : ""
-              }`}
-          >
+          <td className={`table-deadline ${task.completed ? "task-completed" : ""}`}>
             {task.deadline}
           </td>
-          <td
-            className={`table-importance ${task.completed ? "task-completed" : ""
-              }`}
-          >
+          <td className={`table-importance ${task.completed ? "task-completed" : ""}`}>
             {task.important}
           </td>
-          <td
-            className={`table-modify ${task.completed ? "task-completed" : ""}`}
-          >
+          <td className={`table-modify ${task.completed ? "task-completed" : ""}`}>
             <Button
               className="button-icon button-icon-delete-icon"
               onClick={() => deleteTodoItem(task.id)}
@@ -110,12 +116,10 @@ function TodoList({ toggleAddItemForm }) {
       );
     }) || [];
 
-  /**
-   * hàm handleCompleteCheck xử lý việc check hoàn thành
-   */
-  const handleCompleteCheck = (id, completed) => {
-    dispatch(toggleComplete({ id, completed }));
-  };
+    const pagesListRender = pages.map((page) => {
+      return(
+        <Pagination.Item key={page} active={currentPage === page} onClick={()=>handleCurrentPage(page)}>{page}</Pagination.Item>
+      )}) 
 
   return (
     <>
@@ -143,21 +147,18 @@ function TodoList({ toggleAddItemForm }) {
               <tr>
                 <td colSpan={7}>
                   <Pagination>
-                    <Pagination.First />
-                    <Pagination.Prev />
-                    <Pagination.Item>{1}</Pagination.Item>
-                    <Pagination.Ellipsis />
-
-                    <Pagination.Item>{10}</Pagination.Item>
-                    <Pagination.Item>{11}</Pagination.Item>
-                    <Pagination.Item active>{12}</Pagination.Item>
-                    <Pagination.Item>{13}</Pagination.Item>
-                    <Pagination.Item disabled>{14}</Pagination.Item>
-
-                    <Pagination.Ellipsis />
-                    <Pagination.Item>{20}</Pagination.Item>
-                    <Pagination.Next />
-                    <Pagination.Last />
+                    <Pagination.First onClick={() => {if(currentPage!==1) handleCurrentPage(1)}}/>
+                    <Pagination.Prev onClick={() => {if(currentPage>1) handleCurrentPage(currentPage-1)}}/>
+                    {pagesListRender}
+                    <Pagination.Next onClick={() => {if(currentPage<numberOfPages) handleCurrentPage(currentPage+1)}}/>
+                    <Pagination.Last onClick={() => {if(currentPage!==numberOfPages) handleCurrentPage(numberOfPages)}}/>
+                    <Form >
+                      <Form.Select onChange={(e) => {handleNumberItemAPage(e.target.value)}}>
+                        <option value="5">5</option>
+                        <option value="7">7</option>
+                        <option value="10">10</option>
+                      </Form.Select>
+                    </Form>
                   </Pagination>
                 </td>
               </tr>
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,16 +1,38 @@
 import axios from "axios";
 const baseURL = "http://localhost:3001/";
 
-export const getTaskList = async () => {
+/**
+ * Hàm getTaskList get api trả về toàn bộ task trong db
+ * @returns trả về data: 1 mảng các task
+ */
+export const getTaskList = async() => {
   let response;
   try {
     response = await axios.get(baseURL + "taskList");
+    return response.data;
   } catch (error) {
     console.log(error);
   }
-  return response.data;
 };
 
+/**
+ * Hàm getATask get api để trả về 1 task theo id tương ứng
+ * @param {*} id : id của task cần lấy
+ * @returns 1 task có id tương ứng
+ */
+export const getATask = async(id) => {
+  try {
+    let response = await axios.get(baseURL + "taskList" + id);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/**
+ * Hàm deleteTask được sử dụng để xóa 1 nhiệm vụ 
+ * @param {*} id : id của task được chọn để xóa
+ */
 export const deleteTask = async (id) => {
   try {
     await axios.delete(`${baseURL}taskList/${id}`);    
@@ -19,6 +41,10 @@ export const deleteTask = async (id) => {
   }
 };
 
+/**
+ * hàm addTask thêm mới 1 nhiệm vụ
+ * @param {*} task: task mới được thêm vào  
+ */
 export const addTask = async (task) => {
   try {
     await axios.post(baseURL + "taskList", {
@@ -34,3 +60,26 @@ export const addTask = async (task) => {
     console.log(error);
   }
 };
+
+/**
+ * hàm toggleCheckBox bật tắt trạng thái hoàn thành (completed) của task
+ * khi sử dụng patch thì chỉ thay đổi trường được chỉ định
+ * nếu dùng put thì sẽ thay thế toàn bộ task, trường nào ko được chỉ định sẽ bị xóa
+ * @param {*} id : id của task được chọn
+ * @param {*} isChecked : trạng thái được check hay chưa của ô checkbox
+ */
+export const toggleCheckBox = async(id, isChecked) => {
+  try {
+    await axios.patch(baseURL + "taskList/" + id, {completed: !isChecked});
+  } catch (error) {
+    console.log(error);    
+  }
+}
+
+export const toggleCheckBoxAll = async(numberOfTasks) => {
+  try {
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
